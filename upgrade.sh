0759a13326b45d3feaeb914acba26e76f4c2b541
German2404 <gcarvajal2404@gmail.com>
2020-12-09 17:34:58 +0000

    Add script for creating and upgrading helm

--- upgrade.sh
+++ upgrade.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+number=$RANDOM
 echo "Generando nueva imagen con los cambios"
 echo "."
 echo ".."
-docker build --tag german2404/counter:latest
+docker build --tag german2404/counter:latest .
 echo "Subiendo los cambios al repositorio "
 echo "."
 echo ".."
@@ -10,7 +11,7 @@ docker push german2404/counter:latest
 echo "desplegando los cambios en la aplicación"
 echo "."
 echo ".."
-helm upgrade pythonapp ~/sd-project/sd-project
+helm upgrade pythonapp ~/sd-project/sd-project --debug --recreate-pods
 echo "..."
 echo "App web en python desplegada junto a la base de datos"
 sleep 3
