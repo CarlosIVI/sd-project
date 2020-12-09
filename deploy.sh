6214cd2217cbe93cbcab9f9c1905a7181a3aacbd
SebastianUrbano <john.urbano@correo.icesi.edu.co>
2020-12-09 16:32:45 +0000

    add script to automate deploy and upgrade

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "desplegando la aplicación"
+echo "."
+echo ".."
+helm install pythonapp ~/sd-project/sd-project
+echo "..."
+echo "App web en python desplegada junto a la base de datos"
+echo "La aplicación está lista para ser probada en la ip publica del balanceador que se muestra a continuación"
+sleep 120
+kubectl get ingress
+echo "Ingresar a la ip publica, la ruta / muestra la aplicación que cuenta el numero de veces que se ingresa a la app y /health que el microservicio se encuentra arriba"
+
---
+++ upgrade.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Generando nueva imagen con los cambios"
+echo "."
+echo ".."
+docker build --tag german2404/counter:latest
+echo "Subiendo los cambios al repositorio "
+echo "."
+echo ".."
+docker push german2404/counter:latest
+echo "desplegando los cambios en la aplicación"
+echo "."
+echo ".."
+helm upgrade pythonapp ~/sd-project/sd-project
+echo "..."
+echo "App web en python desplegada junto a la base de datos"
+sleep 3
+kubectl get ingress
+echo "Ingresar a la ip publica, la ruta / muestra la aplicación que cuenta el numero de veces que se ingresa a la app y /health que el microservicio se encuentra arriba"
+
