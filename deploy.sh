d12bc19a28558cd2d6156da928c56cf249870cd6
MateoMatta <mateomatta14@hotmail.com>
2020-12-10 17:56:57 +0000

    Fix AutomationScript

--- deploy.sh
+++ deploy.sh
@@ -28,6 +28,6 @@ curl -I http://${EXTERNAL_IP}
 echo "....."
 echo "Pruebas de ejecución de algunos de los Pods que prestan componen el Servicio"
 
-POD_NAME=$(kubectl get pods  -o jsonpath="{.items[0].metadata.name}")kubectl logs $POD_NAME
+POD_NAME=$(kubectl get pods  -o jsonpath="{.items[0].metadata.name}")
 kubectl logs $POD_NAME
 echo "---"
