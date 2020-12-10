b273e426dffd1939c75bde19a59fbf1a5706f34b
CarlosIVI <calitos4488@gmail.com>
2020-12-10 22:58:59 +0000

    fix test and add smoke test

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
 import unittest
 import app as flask_app
-
+import os
 
 class TestApp(unittest.TestCase):
-    
+
     def test_health(self):
         test1 = flask_app.health()
         self.assertEqual(test1,'Microservice a is ready!')
+        print('OK')
 
 if __name__ == "__main__":
     unittest.main()
